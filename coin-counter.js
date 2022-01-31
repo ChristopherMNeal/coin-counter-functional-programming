8d6a901c825c6bb82530e6e2a961e4c2259a1538
Christopher Neal <christopher.m.neal@gmail.com>
2022-01-31 10:51:06 -0800

    refactor to use fewer if statements
    Co-authored-by: Anna Pittman <annaepittman@gmail.com>

--- coin-counter.js
+++ coin-counter.js
@@ -9,33 +9,43 @@
 
 // console.log(countDown(10));
 
-const coinCounter = (amount, quarter, dime, nickle, penny, arr) => {
+const coinCounter = (amount, coins, arr, n) => {
   if (isNaN(amount)) {
     return;
   }
   if (amount <= 0) {
-    console.log(`array ${arr}`);
-    return amount;
-  } else if (amount >= quarter) {
-    arr[0][1]++;
-    return coinCounter(amount - quarter, quarter, dime, nickle, penny, arr);
-  } else if (amount >= dime) {
-    arr[1][1]++;
-    return coinCounter(amount - dime, quarter, dime, nickle, penny, arr);
-  } else if (amount >= nickle) {
-    arr[2][1]++;
-    return coinCounter(amount - nickle, quarter, dime, nickle, penny, arr);
+    return arr;
+  } else if (amount >= coins[0]) {
+    arr[n][1]++;
+    return coinCounter(amount - coins[0], coins, arr, n);
   } else {
-    arr[3][1]++;
-    return coinCounter(amount - penny, quarter, dime, nickle, penny, arr);
+    n++;
+    return coinCounter(amount, coins.slice(1), arr, n);
   }
 };
 
 const arr = [
-  ["quarter", 0],
+  ["quarter", 1],
   ["dime", 0],
-  ["nickle", 0],
-  ["penny", 0],
+  ["nickle", 1],
+  ["penny", 4],
 ];
 
-console.log(coinCounter(199, 25, 10, 5, 1, arr));
+console.log(coinCounter(34, [25, 10, 5, 1], arr, 0));
+
+// function change(amount, coins) {
+//   if (amount == 0) {
+//     return 0;
+//   } else if (coins.length == 0 && amount > 0) {
+//     return Infinity;
+//   } else if (coins[0] > amount) {
+//     return change(amount, coins.slice(1));
+//   } else {
+//     var loseIt = 0 + change(amount, coins.slice(1));
+//     var useIt = 1 + change(amount - coins[0], coins);
+//     return Math.min(loseIt, useIt);
+//   }
+// }
+
+// console.log(change(101, [1, 5, 10, 25]));
+// 6
